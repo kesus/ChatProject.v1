5d675b4ee107bab60625e21fbadb9f3de56e5cbe
kesus <ikesus@icloud.com>
2016-12-14 10:55:44 +0000

    Add files via upload

---
+++ src/client/Login.java
@@ -0,0 +1,31 @@
+package client;
+
+/**
+ * Created by Keno on 12/13/2016.
+ */
+public class Login {
+
+
+
+    private void loginAccount() {
+
+        // Checks if username exists & if pass is correct.
+        // Send message to user if pass is wrong or if user doesn't exist.
+
+    }
+
+
+    private boolean userExist() {
+
+        return true;
+    }
+
+
+
+    /***
+     * Code for GUI can go here.
+      */
+
+
+
+}
---
+++ src/client/Register.java
@@ -0,0 +1,19 @@
+package client;
+
+/**
+ * Created by Keno on 12/13/2016.
+ */
+public class Register {
+
+    /*** For this class we'll just be creating the account
+     *  and checking if it exists
+     *
+     */
+
+
+
+    /***
+     * Code for the GUI goes here...
+     */
+
+}
